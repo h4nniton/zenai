cf7f7643cad6bb716dba7dbdc7c8ad55f4edb9bb
Emily Crepaldi <emily.ccarvalho13@gmail.com>
2024-04-25 16:44:57 -0300

    25/04 - terminando tela login

---
+++ login.js
@@ -0,0 +1,31 @@
+const button = document.getElementById('login')
+
+async function validarLogin(){
+
+    const email = document.getElementById('email').value
+    const senha = document.getElementById('senha').value
+
+    console.log(email)
+
+    if(email === '' || senha === ''){
+        alert('Preencha os Campos Corretamente....')
+    } else {
+
+        const users = await fetch('http://localhost:5080/usuario') 
+        const listUsers = await users.json()
+
+        listUsers.forEach((user) => {
+            if(email === user.email && senha === user.senha){
+
+                const userId = user.id
+                localStorage.setItem('userId', userId)
+                alert('Usuário Logado com Sucesso !!!')
+                window.location.href = '../telahome.html'
+            }
+        })
+    }
+}
+
+window.onload = () => {
+    button.addEventListener('click', validarLogin)
+}
